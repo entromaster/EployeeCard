d0c0667696a95af684f2ec2a881457a0095db0b0
Suman Saurav <sumansaurav0243@gmail.com>
2023-02-11 01:24:52 +0530

    Implemented Routers

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import { v4 as uuidv4 } from "uuid";
 import EditEmployee from "./components/EditEmployee";
 import Header from "./components/Header";
 import Employees from "./pages/Employees";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  
-
-  
   return (
-    <Header>
-      <Employees/>
-    </Header>
-  )
+    <BrowserRouter>
+      <Header>
+        <Routes>
+          <Route path="/" element={<Employees />} />
+        </Routes>
+      </Header>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { NavLink } from "react-router-dom";
 
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
+  { name: "Employees", href: "/Employees" },
+  { name: "Customers", href: "#" },
+  { name: "Projects", href: "#" },
+  { name: "Calendar", href: "#" },
 ];
 
 function classNames(...classes) {
@@ -35,19 +36,26 @@ export default function Header(props) {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <NavLink
                         key={item.name}
-                        href={item.href}
-                        className={classNames(
+                        to={item.href}
+                        /*className={classNames(
                           item.current
-                            ? "no-underline bg-gray-900 text-white"
-                            : "no-underline text-gray-300 hover:bg-gray-700 hover:text-white",
-                          "px-3 py-2 rounded-md text-sm font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
+                            ? "no-underline "
+                            : "no-underline ",
+                          ""
+                        )}*/
+                        className={({ isActive }) => {
+                          return (
+                            "px-3 py-2 rounded-md text-sm font-medium no-underline " +
+                            (!isActive
+                              ? "bg-gray-900 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white")
+                          );
+                        }}
                       >
                         {item.name}
-                      </a>
+                      </NavLink>
                     ))}
                   </div>
                 </div>
